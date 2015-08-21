31320e5239067d0e6355dcaae8d8c04f8af8c157
daiane <daianefs.ciandt.com>
2015-08-21 13:42:39 -0300

    Challenge friday complete

--- worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/LoginActivity.java
+++ worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/LoginActivity.java
@@ -1,8 +1,15 @@
 package com.ciandt.worldwonders;
 
+import com.ciandt.worldwonders.model.User;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.Serializable;
 
 /**
  * Created by jpimentel on 8/20/15.
@@ -11,12 +18,39 @@ public class LoginActivity extends AppCompatActivity {
 
     final static String TAG = "LoginActivity";
 
+
+    Button loginButton, signupButton;
+    EditText usernameEditText, passwordEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.i(TAG, "onCreate");
 
         setContentView(R.layout.activity_login);
+
+        loginButton = (Button) findViewById(R.id.btn_login);
+        signupButton = (Button) findViewById(R.id.btn_signup);
+        usernameEditText = (EditText) findViewById(R.id.input_user_name);
+        passwordEditText = (EditText) findViewById(R.id.input_password);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data != null) {
+            User u = (User) data.getSerializableExtra("user_info");
+            usernameEditText.setText(u.user);
+        }
     }
 
     @Override
