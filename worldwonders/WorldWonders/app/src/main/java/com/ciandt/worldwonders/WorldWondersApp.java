b3fefcf7c8ad80ce15f5b7e85c785a5ef3e2d4e3
Péricles Lopes Machado <pericles.raskolnikoff@gmail.com>
2015-08-24 17:03:17 -0300

    initial code to DAO

--- worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/WorldWondersApp.java
+++ worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/WorldWondersApp.java
@@ -2,8 +2,15 @@ package com.ciandt.worldwonders;
 
 import android.app.Application;
 
+import com.ciandt.worldwonders.helper.WondersSQLiteHelper;
+
 /**
  * Created by pmachado on 8/24/15.
  */
 public class WorldWondersApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        WondersSQLiteHelper.initialize();
+    }
 }
