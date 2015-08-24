7d6d15851661805111c8956f7cf1ca81515ed645
Péricles Lopes Machado <pericles.raskolnikoff@gmail.com>
2015-08-24 20:51:27 -0300

    Update Wonder.java

--- worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/model/Wonder.java
+++ worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/model/Wonder.java
@@ -2,6 +2,11 @@ package com.ciandt.worldwonders.model;
 
 import java.io.Serializable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by pmachado on 8/23/15.
  */
@@ -21,7 +26,20 @@ public class Wonder implements Serializable {
     public double latitude;
     public double longitude;
 
-
+    static Wonder fromHashMap(HashMap<String, Object> table) {
+        Wonder w = new Wonder(table.get("name"), table.get("id"), table.get("description"), 
+        table.get("url"), table.get("photo"), table.get("longitude"), table.get("latitude"));
+        return w;
+    }
+    
+    static List<Wonder> fromListHashMap(List<HashMap<String, Object>> listTable) {
+        List<Wonder> l = new ArrayList<Wonder>();
+        for (HashMap<String, Object> t: listTable) {
+            l.add(fromHashMap(t));
+        }
+        return l;
+    }
+    
     public Wonder(String name, int id, String description, String url, String photo, double longitude, double latitude) {
         this.name = name;
         this.id = id;
