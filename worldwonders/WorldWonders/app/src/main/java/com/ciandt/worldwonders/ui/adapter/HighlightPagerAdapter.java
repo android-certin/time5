11f74c5772f27fa28ce91b49657e7d3b7ed9b836
enirsilvaferraz <enirsilvaferraz@gmail.com>
2015-08-25 11:26:22 -0300

    Ajustes no banco

--- worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/ui/adapter/HighlightPagerAdapter.java
+++ worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/ui/adapter/HighlightPagerAdapter.java
@@ -15,30 +15,18 @@ import java.util.List;
  * Created by pmachado on 8/23/15.
  */
 public class HighlightPagerAdapter extends FragmentStatePagerAdapter {
-    private WondersRepository repository;
+
     private List<Wonder> listWonders;
 
-    public HighlightPagerAdapter(FragmentManager fm, Context context) {
+    public HighlightPagerAdapter(FragmentManager fm, List<Wonder> listWonders) {
         super(fm);
-        repository = new WondersRepository(context);
-        repository.getAll(new WondersRepository.WonderAllListener() {
-            @Override
-            public void onWonderAll(Exception e, List<Wonder> wonders) {
-                HighlightPagerAdapter.this.listWonders = wonders;
-            }
-        });
+this.listWonders = listWonders;
     }
 
     @Override
     public Fragment getItem(int position) {
 
-        Wonder wonder = new Wonder("mausoleu.jpg",0);
-        wonder.photo = "mausoleu.jpg";
-
-        if (listWonders != null && listWonders.size()>position){
-           wonder = listWonders.get(position);
-        }
-
+        Wonder wonder = listWonders.get(position);
         HighlightFragment highlightFragment = HighlightFragment.newInstance(wonder);
         return highlightFragment;
     }
--- worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/ui/fragment/WondersFragment.java
+++ worldwonders/WorldWonders/app/src/main/java/com/ciandt/worldwonders/ui/fragment/WondersFragment.java
@@ -1,9 +1,13 @@
 package com.ciandt.worldwonders.ui.fragment;
 
 import com.ciandt.worldwonders.R;
+import com.ciandt.worldwonders.model.Wonder;
+import com.ciandt.worldwonders.repository.WondersRepository;
 import com.ciandt.worldwonders.ui.adapter.HighlightPagerAdapter;
 
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
@@ -11,10 +15,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class WondersFragment extends android.support.v4.app.Fragment {
+import java.util.List;
+
+public class WondersFragment extends Fragment {
 
     private ViewPager viewPager;
     private PagerAdapter pagerAdapter;
+
+    private FragmentManager manager;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,10 +34,24 @@ public class WondersFragment extends android.support.v4.app.Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_wonders, container, false);
 
-        viewPager = (ViewPager) v.findViewById(R.id.slide);
-        pagerAdapter = new HighlightPagerAdapter(getActivity().getSupportFragmentManager(), getContext());
-        viewPager.setAdapter(pagerAdapter);
-
         return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewPager = (ViewPager) view.findViewById(R.id.slide);
+       manager  = getFragmentManager();
+
+        WondersRepository repository = new WondersRepository(getContext());
+        repository.getAll(new WondersRepository.WonderAllListener() {
+            @Override
+            public void onWonderAll(Exception e, List<Wonder> wonders) {
+
+                pagerAdapter = new HighlightPagerAdapter(manager, wonders);
+                viewPager.setAdapter(pagerAdapter);
+            }
+        });
     }
 }
